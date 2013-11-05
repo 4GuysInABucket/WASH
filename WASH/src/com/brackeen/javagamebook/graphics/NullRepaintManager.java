1a65b1c628ec8358a7ca08c89f532bd66056a0e4
ilianagh <ilianagh@10.20.9.173>
2013-11-05 12:28:59 -0600

    Comentarios en NullRepaintManager (graphics)

--- WASH/src/com/brackeen/javagamebook/graphics/NullRepaintManager.java
+++ WASH/src/com/brackeen/javagamebook/graphics/NullRepaintManager.java
@@ -4,35 +4,54 @@ import javax.swing.RepaintManager;
 import javax.swing.JComponent;
 
 /**
-    The NullRepaintManager is a RepaintManager that doesn't
-    do any repainting. Useful when all the rendering is done
-    manually by the application.
-*/
+ * The NullRepaintManager is a RepaintManager that doesn't
+ * do any repainting. Useful when all the rendering is done
+ * manually by the application.
+ */
 public class NullRepaintManager extends RepaintManager {
 
     /**
-        Installs the NullRepaintManager.
-    */
+     * Installs the NullRepaintManager.
+     */
     public static void install() {
         RepaintManager repaintManager = new NullRepaintManager();
         repaintManager.setDoubleBufferingEnabled(false);
         RepaintManager.setCurrentManager(repaintManager);
     }
-
+    
+    /**
+     * Sets a JComponent as invalid.
+     * @param c  the JComponent
+     */
     public void addInvalidComponent(JComponent c) {
         // do nothing
     }
-
+    
+    /**
+     * Sets a region as dirty.
+     * @param c  the JComponent
+     * @param x  the position in x
+     * @param y  the position in e y
+     * @param w  the width
+     * @param h  the height
+     */
     public void addDirtyRegion(JComponent c, int x, int y,
         int w, int h)
     {
         // do nothing
     }
 
+    /**
+     * Sets a JComponent as completely dirty.
+     * @param c  the JComponent
+     */
     public void markCompletelyDirty(JComponent c) {
         // do nothing
     }
 
+    /**
+     * Paints dirty regions.
+     */
     public void paintDirtyRegions() {
         // do nothing
     }
