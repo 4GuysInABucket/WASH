307c8660169fc3de2f1fb54d62242017440463f9
ilianagh <ilianagh@10.20.9.173>
2013-11-24 13:11:58 -0600

    score sin loop

--- WASH/src/com/brackeen/javagamebook/tilegame/WashStart.java
+++ WASH/src/com/brackeen/javagamebook/tilegame/WashStart.java
@@ -103,6 +103,7 @@ public class WashStart extends GameCore {
     private LinkedList<Integer> scorelist; 
     private String fileName;
     private String[] arr;
+    private boolean bscores;
     
     public static Image iPause;
     public static Image iGameOver;
@@ -155,6 +156,7 @@ public class WashStart extends GameCore {
         
         fileName = "scores.txt";
         scorelist = new LinkedList<Integer>();
+        bscores = false;
     }
     
     /**
@@ -206,6 +208,7 @@ public class WashStart extends GameCore {
         bPause = false;
         
         scorelist = new LinkedList<Integer>();
+        bscores = false;
     }
     
     /**
@@ -472,13 +475,13 @@ public class WashStart extends GameCore {
             checkBulletCollision();
         }
         
-        if (lives<=0) {
+        if (lives<=0 && !bscores) {
             scorelist.add(score);
             
             try {
                 readFile();
-                scorelist.add(score);
                 saveFile();
+                bscores = true;
             } catch (IOException ex) {
                 System.out.println("Error in " + ex.toString());
             }
